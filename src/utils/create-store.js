f6605d15e60ed95f0200ba86f39224e3f072870e
Yusuf <hello@iamyuu.dev>
2021-08-15 23:03:28 +0700

    refactory(store): add log middleware & pipe middleware

--- src/utils/create-store.js
+++ src/utils/create-store.js
@@ -1,9 +1,29 @@
 import create from 'zustand'
 import {devtools} from 'zustand/middleware'
+import {pipe} from './misc'
+
+const log = config => (set, get, api) => {
+  return config(
+    args => {
+      const filterState = state =>
+        Object.keys(state)
+          .filter(key => typeof state[key] !== 'function')
+          .reduce((res, key) => Object.assign(res, {[key]: state[key]}), {})
+
+      window.console.groupCollapsed('%c--- logger ---', 'font-weight: bold;')
+      window.console.log('%cprev state', 'color: #00AFF8;', filterState(get()))
+      set(args)
+      window.console.log('%cnext state', 'color: #4AB14D;', filterState(get()))
+      window.console.groupEnd()
+    },
+    get,
+    api,
+  )
+}
 
 const createStore = store => {
   if (import.meta.env.DEV) {
-    return create(devtools(store))
+    return pipe(log, devtools, create)(store)
   }
 
   return create(store)
--- src/utils/misc.js
+++ src/utils/misc.js
@@ -5,3 +5,7 @@ export function uuidv4() {
     return v.toString(16)
   })
 }
+
+export function pipe(...fns) {
+  return fns.reduce((a, b) => arg => b(a(arg)))
+}
