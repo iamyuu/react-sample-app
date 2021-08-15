ce4aa41f22c4d42fbf1c49b5795b4b66d24f0bde
Yusuf <hello@iamyuu.dev>
2021-08-15 10:47:03 +0700

    feat: persist todo

--- src/store/todo/store.js
+++ src/store/todo/store.js
@@ -1,25 +1,33 @@
+import {persist} from 'zustand/middleware'
 import createStore from '~/utils/create-store'
 import {uuidv4} from '~/utils/misc'
 
-export const useTodo = createStore(set => ({
-  todos: [],
-  addTodo: title => {
-    const newTodo = {
-      id: uuidv4(),
-      title,
-    }
+export const useTodo = createStore(
+  persist(
+    set => ({
+      todos: [],
+      addTodo: title => {
+        const newTodo = {
+          id: uuidv4(),
+          title,
+        }
 
-    set(prevState => ({
-      todos: [...prevState.todos, newTodo],
-    }))
-  },
-  removeTodo: id => {
-    set(prevState => {
-      const newTodos = prevState.todos.filter(val => val.id !== id)
+        set(prevState => ({
+          todos: [...prevState.todos, newTodo],
+        }))
+      },
+      removeTodo: id => {
+        set(prevState => {
+          const newTodos = prevState.todos.filter(val => val.id !== id)
 
-      return {todos: newTodos}
-    })
-  },
-}))
+          return {todos: newTodos}
+        })
+      },
+    }),
+    {
+      name: 'react-sample:todo',
+    },
+  ),
+)
 
 export default useTodo
