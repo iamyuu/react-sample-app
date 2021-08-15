e399267dacf7a32fddffa53351c815d14e1399b2
Yusuf <hello@iamyuu.dev>
2021-08-15 10:46:52 +0700

    chore: only use zustand devtools on dev

--- src/utils/create-store.js
+++ src/utils/create-store.js
@@ -1,16 +1,12 @@
 import create from 'zustand'
 import {devtools} from 'zustand/middleware'
 
-const log = config => (set, get, api) =>
-  config(
-    args => {
-      console.log('  applying', args)
-      set(args)
-      console.log('  new state', get())
-    },
-    get,
-    api,
-  )
+const createStore = store => {
+  if (import.meta.env.DEV) {
+    return create(devtools(store))
+  }
 
-const createStore = store => create(devtools(store))
+  return create(store)
+}
+
 export default createStore
